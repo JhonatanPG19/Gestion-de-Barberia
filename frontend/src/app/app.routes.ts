93b703bc1a5fa9fbbf6efdd213181b2302078cd4
Adrian Camilo <adcamilox2000@gmail.com>
2025-11-30 13:00:07 -0500

    primer versionde front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'reservas'
+	},
+	{
+		path: 'reservas',
+		loadChildren: () => import('./features/reservas/reservas.routes').then((m) => m.RESERVAS_ROUTES)
+	},
+	{
+		path: '**',
+		redirectTo: 'reservas'
+	}
+];
