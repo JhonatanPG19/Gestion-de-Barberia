1965d01541e89e5af5322f3787e9d915a6d29ba9
Adrian Camilo <adcamilox2000@gmail.com>
2025-12-03 11:21:41 -0500

    mini fix

--- ms-usuarios/src/main/java/com/barberia/ms_usuarios/capaAccesoADatos/repository/IUsuarioRepository.java
+++ ms-usuarios/src/main/java/com/barberia/ms_usuarios/capaAccesoADatos/repository/IUsuarioRepository.java
@@ -1,19 +1,14 @@
-package com.barberia.ms_usuarios.capaAccesoADatos.repository;
-
-import com.barberia.ms_usuarios.dominio.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
-    // Spring Data JPA implementa esto automáticamente en tiempo de ejecución
-    boolean existsByUsername(String username);
-    boolean existsByCorreo(String correo);
-    Optional<Usuario> findByUsername(String username);
-<<<<<<< HEAD
-=======
-
->>>>>>> 263d880c091acb7631fcb75c0b143f3acbead048
+package com.barberia.ms_usuarios.capaAccesoADatos.repository; 
+ 
+import com.barberia.ms_usuarios.dominio.Usuario; 
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.stereotype.Repository; 
+ 
+import java.util.Optional; 
+ 
+@Repository 
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> { 
+    boolean existsByUsername(String username); 
+    boolean existsByCorreo(String correo); 
+    Optional<Usuario> findByUsername(String username); 
 }
