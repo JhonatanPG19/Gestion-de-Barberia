1b47b7d840af4475c953884018ef9d36983b86b0
Adrian Camilo <adcamilox2000@gmail.com>
2025-12-03 11:22:13 -0500

    final

--- ms-usuarios/src/main/java/com/barberia/ms_usuarios/capaAccesoADatos/repository/IUsuarioRepository.java
+++ ms-usuarios/src/main/java/com/barberia/ms_usuarios/capaAccesoADatos/repository/IUsuarioRepository.java
@@ -1,14 +1,15 @@
-package com.barberia.ms_usuarios.capaAccesoADatos.repository; 
- 
-import com.barberia.ms_usuarios.dominio.Usuario; 
-import org.springframework.data.jpa.repository.JpaRepository; 
-import org.springframework.stereotype.Repository; 
- 
-import java.util.Optional; 
- 
-@Repository 
-public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> { 
-    boolean existsByUsername(String username); 
-    boolean existsByCorreo(String correo); 
-    Optional<Usuario> findByUsername(String username); 
+package com.barberia.ms_usuarios.capaAccesoADatos.repository;
+
+import com.barberia.ms_usuarios.dominio.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+    // Spring Data JPA implementa esto automáticamente en tiempo de ejecución
+    boolean existsByUsername(String username);
+    boolean existsByCorreo(String correo);
+    Optional<Usuario> findByUsername(String username);
 }
